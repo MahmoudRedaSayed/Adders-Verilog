310dd42f9d3c2998b7411eabdd493dcdc5943bf4
Ahmed-H300 <ahmed300hany@gmail.com>
2022-10-30 23:54:32 +0200

    carry save adder modified to sum 2 and over flow is added for carry save and carry select

--- project/carry_select_adder.v
+++ project/carry_select_adder.v
@@ -1,5 +1,5 @@
 `include "carry_select_adder_4bit.v"
-module carrySelectAdder #(parameter N=32)(input [N-1:0] x, input [N-1:0] y, input carryin, output [N-1:0] sum, output carryout);
+module carrySelectAdder #(parameter N=32)(input [N-1:0] x, input [N-1:0] y, input carryin, output [N-1:0] sum, output carryout, output overflow);
 
 wire [(N/4):0] c;
 
@@ -12,5 +12,7 @@ carrySelectAdder4bit SA(x[((i*4)+3):(i*4)], y[((i*4)+3):(i*4)], c[i], sum[((i*4)
 endgenerate
 
 assign carryout = c[(N/4)];
+
+assign overflow = carryout ^ sum[N-1];
 
 endmodule
